69528cc7ffc9b678aaf6d3aeb9863f0d841cc307
efebey5376 <yigitefeyelkenci@gmail.com>
2025-01-09 09:47:17 +0300

    Update heart.py

--- heart.py
+++ heart.py
@@ -3,7 +3,7 @@ import turtle
 def xt(t):
     return 16*math.sin(t)**3
 def yt(t):
-    return 13*math.cos(t)-5*\
+    return 13*math.cos(t)-5*\   
         math.cos(2*t)-2*\
             math.cos(3*t)-\
                 math.cos(4*t)
